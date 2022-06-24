965d80214c893f3b5fcdeead1f8e3b78158cae82
qingzhixing <qzsptmail@protonmail.com>
2022-06-25 01:18:13 +0800

    791. 高精度加法

--- 第一讲 基础算法/高精度/791. 高精度加法.cpp
+++ 第一讲 基础算法/高精度/791. 高精度加法.cpp
@@ -3,16 +3,46 @@
 #include<string>
 using namespace std;
 
+//Char vector从右往左读string,高位在末尾
 vector<char> StringToCharVector(const string& str){
-
+    vector<char> number;
+    for(int i=str.size()-1;i>=0;i--){
+        number.push_back(str[i]-'0');
+    }
+    return move(number);
 }
 
 string CharVectorToString(const vector<char>& number){
-
+    string str;
+    for(auto item:number){
+        str=(char(item+'0'))+str;
+    }
+    return move(str);
 }
 
 vector<char> CharVectorAdd(const vector<char>& number1,const vector<char>& number2){
-    
+    vector<char> answer;
+    int pointer=0;
+    int carry=0;
+    while(pointer<=number1.size()-1||pointer<=number2.size()-1){
+        int digitSum=0;
+        if(pointer<=number1.size()-1){
+            digitSum+=number1[pointer];
+        }
+        if(pointer<=number2.size()-1){
+            digitSum+=number2[pointer];
+        }
+        digitSum+=carry;
+        carry=digitSum/10;
+        digitSum%=10;
+        answer.push_back(digitSum);
+        pointer++;
+    }
+    if(carry){
+        answer.push_back(carry);
+        carry=0;
+    }
+    return move(answer);
 }
 
 int main(){
@@ -21,6 +51,12 @@ int main(){
     cin>>str1>>str2;
     number1=StringToCharVector(str1);
     number2=StringToCharVector(str2);
-    string answer = CharVectorToString(CharVectorAdd(number1,number2));
-    cout<<answer<<endl;
+    vector<char> answer=CharVectorAdd(number1,number2);
+
+    //debug:
+    // cout<<CharVectorToString(number1)<<endl;
+    // cout<<CharVectorToString(number2)<<endl;
+
+    string answerStr = CharVectorToString(answer);
+    cout<<answerStr<<endl;
 }
