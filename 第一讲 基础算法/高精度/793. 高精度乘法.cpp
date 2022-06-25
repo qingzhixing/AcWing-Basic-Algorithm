0264e70ea7deeca45905bbe7b0aff3c740cf6818
qingzhixing <qzsptmail@protonmail.com>
2022-06-26 00:31:53 +0800

    Finished. 793. 高精度乘法

--- 第一讲 基础算法/高精度/793. 高精度乘法.cpp
+++ 第一讲 基础算法/高精度/793. 高精度乘法.cpp
@@ -21,3 +21,33 @@ string CharVectorToString(const CharVec& number){
     }
     return move(str);
 }
+
+//copied from 791. 高精度加法.cpp
+CharVec CharVectorMultipleInteger(const CharVec& number,int multiple){
+    int carry=0;
+    CharVec result;
+    for(int i=0;i<number.size();i++){
+        int temp=number[i]*multiple+carry;
+        carry=temp/10;
+        result.push_back(temp%10);
+    }
+    while(carry){
+        result.push_back(carry%10);
+        carry/=10;
+    }
+    //去除前导0
+    while(result.size()>1&&result.back()==0){
+        result.pop_back();
+    }
+    return move(result);
+}
+
+int main(){
+    string numberStr;
+    int multiple;
+    cin>>numberStr>>multiple;
+    CharVec number=StringToCharVector(numberStr);
+    CharVec result=CharVectorMultipleInteger(number,multiple);
+    cout<<CharVectorToString(result)<<endl;
+    return 0;
+}
